a507fe82b7bae1f4a7a75cc0393714d9cdff2a22
unknown <svetikons@mail.ru>
2021-02-02 21:44:19 +0300

    Правки: Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const COMMENT_LENGTH = 140;
+let text = '';
+
+const getRandomNumber = function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const verifyStringLength = function (string, maxLength) {
+  return string.length <= maxLength;
+}
+
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
