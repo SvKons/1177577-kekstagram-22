d61543c7dd884e0e7a83322c1e5920e1d0776f59
unknown <svetikons@mail.ru>
2021-02-03 20:22:31 +0300

    Исправляет замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 'use strict';
-
-const COMMENT_LENGTH = 140;
-let text = '';
-
-const getRandomNumber = function getRandomIntInclusive(min, max) {
+// eslint-disable-next-line
+const COMMENT_LENGTH = 140; // eslint-disable-next-line
+let text = ''; // eslint-disable-next-line
+const getRandomNumber = function getRandomIntInclusive(min, max)  { // eslint-disable-next-line
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const verifyStringLength = function (string, maxLength) {
+} // eslint-disable-next-line
+const verifyStringLength = function (string, maxLength) { // eslint-disable-next-line
   return string.length <= maxLength;
 }
-
-//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
